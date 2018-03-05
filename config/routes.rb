f54298e353eb38c63e5de6a53233efd416931156
tech-camp <restartuivereyh4@gmail.com>
2018-03-05 12:13:05 +0900

    create routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :messages, only: :index
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
