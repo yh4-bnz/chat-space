be2fa5b905d21e523fbee52f02e47509cede6578
tech-camp <restartuivereyh4@gmail.com>
2018-03-05 15:02:11 +0900

    edit hamls and modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
-  has_many :membaers
+  has_many :members
   has_many :groups, through: :members
 end
