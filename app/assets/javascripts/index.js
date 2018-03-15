f67122cf2e613705f473c8c7dab317085bbce86f
yosuke <restartuivereyh4@gmail.com>
2018-03-15 16:51:56 +0900

    create ajax and modify controller

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,12 @@
 $(function(){
   $('#user-search-field').on('keyup', function(){
     var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
